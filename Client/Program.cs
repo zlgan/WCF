0189024d747dffd59d2c8981048db1a26930ab69
zlgan <278408588@qq.com>
2019-03-24 09:56:26 +0800

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.wcf1;
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WCFServiceClient svc = new WCFServiceClient();
+            Console.WriteLine(svc.GetData(666));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
